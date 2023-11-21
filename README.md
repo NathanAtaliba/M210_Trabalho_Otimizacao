d1a4da4e8b002639ec94b80815b1e43209981c0b
Nathan Santos Ataliba <nathan.s@inatel.br>
2023-11-21 11:59:07 -0300

    Simplex :construction:

--- README.md
+++ README.md
@@ -2,9 +2,47 @@
 Repositorio criado para trabalho de otimização
 
 # INTEGRANTES:
-Nathan Santos Ataliba 1663
-Gabriel Guerzony 235
-
+Nathan Santos Ataliba, 1663 , GEC
+Gabriel Guerzoni , 235 , GES
+Marcos Guerra, XXX , XXX
 # LOGICA:
 
+   O método Simplex é um algoritmo utilizado para resolver problemas de programação linear, que são problemas de otimização nos quais se busca maximizar ou minimizar uma função linear sujeita a um conjunto de restrições lineares. Aqui estão os passos básicos para utilizar o método Simplex:
+
+
+## Formule o Problema:
+
+    Defina a função objetivo que você deseja maximizar ou minimizar.
+    Estabeleça as restrições do problema na forma de equações lineares.
+
+## Forma Padrão:
+    Certifique-se de que o problema está na forma padrão, o que significa que a função objetivo é de maximização, todas as variáveis são não negativas e as restrições são todas do tipo ≤.
+
+## Crie a Tabela Simplex Inicial:
+
+    Escreva as equações das restrições e a função objetivo em uma tabela. Adicione variáveis de folga, se necessário.
+
+## Escolha a Variável de Entrada:
+
+    Identifique a variável de decisão que entrará na base. Essa escolha geralmente é feita com base no coeficiente mais negativo na função objetivo.
+
+## Escolha a Variável de Saída:
+
+    Determine qual variável de folga ou de excesso deixará a base. Isso é feito considerando as restrições e garantindo que a solução continue sendo factível.
+
+# Atualize a Tabela Simplex:
+
+    Utilize operações elementares para atualizar a tabela. As operações mais comuns são pivotamento e escalonamento.
+
+# Verifique o Critério de Parada:
+
+Verifique se a solução atual é ótima ou se deve continuar o processo. O critério de parada geralmente envolve todos os coeficientes da função objetivo sendo não negativos.
+
+## Iteração:
+
+Se a solução não for ótima, repita os passos 4 a 7 até atingir a solução ótima.
+
+## Interpretação da Solução:
+
+Após alcançar a solução ótima, interprete os resultados em termos do problema original.
 
---
+++ src/methodSimplex.py
@@ -0,0 +1,188 @@
+#Trabalho de M210 - Simplex
+
+#Importação de bibliotecas 
+import numpy as np
+
+#Obter as dimensões do problema
+num_variables = int(input("Digite o número de variáveis: "))
+num_constraints = int(input("Digite o número de restrições: "))
+
+#Solicitar os coeficientes da função objetivo
+funcObj = []
+
+for i in range(num_variables):
+    coefficient = float(input(f"Digite o coeficiente da variável x{i+1} na função objetivo: "))
+    funcObj.append(-coefficient)
+
+funcObj = np.array(funcObj)
+
+#Solicitar os coeficientes das restrições
+restric = []
+
+for i in range(num_constraints):
+    row = []
+    for j in range(num_variables):
+        coefficient = float(input(f"Digite o coeficiente da variável x{j+1} na restrição {i+1}: "))
+        row.append(coefficient)
+    restric.append(row)
+
+restric = np.array(restric)
+
+#Solicitar os termos constantes das restrições
+const = []
+
+for i in range(num_constraints):
+    constant = float(input(f"Digite o termo constante na restrição {i+1}: "))
+    const.append(constant)
+
+const = np.array(const)
+
+def simplex(funcObj, restricoes, constantes):
+
+    #Quantidade de variáveis e constantes
+    n_vars = len(funcObj)
+    n_cons = len(constantes)
+
+    #Adicionando as variáveis às constantes em um vetor 
+    restricoes = np.hstack([restricoes, np.eye(n_cons)])
+    funcObj = np.hstack([funcObj, np.zeros(n_cons)])
+    
+    #Cria um tableau inicial
+    tableau = np.vstack([np.hstack([restricoes, constantes.reshape(-1, 1)]), np.hstack([funcObj, 0])])
+
+    #Iteração até que o método encontre a solução ótima
+    while True:
+        #Checando se a solução é ótima
+        if np.all(tableau[-1, :-1] >= 0):
+            break
+
+        #Encontrando a coluna pivô
+        pivot_col = np.argmin(tableau[-1, :-1])
+
+        #Encontrando a linha pivô
+        ratios = tableau[:-1, -1] / tableau[:-1, pivot_col]
+        ratios[ratios < 0] = np.inf
+        pivot_row = np.argmin(ratios)
+
+        #Operação que utiliza os valores pivô
+        pivot_val = tableau[pivot_row, pivot_col]
+        tableau[pivot_row] /= pivot_val
+        for i in range(tableau.shape[0]):
+            if i != pivot_row:
+                tableau[i] -= tableau[pivot_row] * tableau[i, pivot_col]
+
+    #Extraindo a solução 
+    solucao = np.zeros(n_vars)
+    for i in range(n_vars):
+        col = tableau[:, i]
+        if (col == 0).sum() == n_cons:
+            row = np.where(col[:-1] == 1)[0][0]
+            solucao[i] = tableau[row, -1]
+
+    #Preço sombra
+    precoSombra = tableau[-1, n_vars:-1]
+
+    return solucao, tableau[-1, -1], precoSombra
+
+
+#Chamada da função simplex para resolver o problema de programação linear 
+solucao, valorOtimo, precoSombra = simplex(funcObj, restric, const)
+
+#Imprimindo os resultados solicitados
+print(f'Solução Ótima: {solucao}')
+print(f'Lucro Ótimo: {valorOtimo}')
+print(f'Preços Sombra: {precoSombra}')#Ana Clara dos Santos Rosa, 15 GES e Julia da Silva Villela, 1799 GEC
+#Trabalho de M210 - Simplex
+
+#Importação de bibliotecas 
+import numpy as np
+
+#Obter as dimensões do problema
+num_variables = int(input("Digite o número de variáveis: "))
+num_constraints = int(input("Digite o número de restrições: "))
+
+#Solicitar os coeficientes da função objetivo
+funcObj = []
+
+for i in range(num_variables):
+    coefficient = float(input(f"Digite o coeficiente da variável x{i+1} na função objetivo: "))
+    funcObj.append(-coefficient)
+
+funcObj = np.array(funcObj)
+
+#Solicitar os coeficientes das restrições
+restric = []
+
+for i in range(num_constraints):
+    row = []
+    for j in range(num_variables):
+        coefficient = float(input(f"Digite o coeficiente da variável x{j+1} na restrição {i+1}: "))
+        row.append(coefficient)
+    restric.append(row)
+
+restric = np.array(restric)
+
+#Solicitar os termos constantes das restrições
+const = []
+
+for i in range(num_constraints):
+    constant = float(input(f"Digite o termo constante na restrição {i+1}: "))
+    const.append(constant)
+
+const = np.array(const)
+
+def simplex(funcObj, restricoes, constantes):
+
+    #Quantidade de variáveis e constantes
+    n_vars = len(funcObj)
+    n_cons = len(constantes)
+
+    #Adicionando as variáveis às constantes em um vetor 
+    restricoes = np.hstack([restricoes, np.eye(n_cons)])
+    funcObj = np.hstack([funcObj, np.zeros(n_cons)])
+    
+    #Cria um tableau inicial
+    tableau = np.vstack([np.hstack([restricoes, constantes.reshape(-1, 1)]), np.hstack([funcObj, 0])])
+
+    #Iteração até que o método encontre a solução ótima
+    while True:
+        #Checando se a solução é ótima
+        if np.all(tableau[-1, :-1] >= 0):
+            break
+
+        #Encontrando a coluna pivô
+        pivot_col = np.argmin(tableau[-1, :-1])
+
+        #Encontrando a linha pivô
+        ratios = tableau[:-1, -1] / tableau[:-1, pivot_col]
+        ratios[ratios < 0] = np.inf
+        pivot_row = np.argmin(ratios)
+
+        #Operação que utiliza os valores pivô
+        pivot_val = tableau[pivot_row, pivot_col]
+        tableau[pivot_row] /= pivot_val
+        for i in range(tableau.shape[0]):
+            if i != pivot_row:
+                tableau[i] -= tableau[pivot_row] * tableau[i, pivot_col]
+
+    #Extraindo a solução 
+    solucao = np.zeros(n_vars)
+    for i in range(n_vars):
+        col = tableau[:, i]
+        if (col == 0).sum() == n_cons:
+            row = np.where(col[:-1] == 1)[0][0]
+            solucao[i] = tableau[row, -1]
+
+    #Preço sombra
+    precoSombra = tableau[-1, n_vars:-1]
+
+    return solucao, tableau[-1, -1], precoSombra
+
+
+#Chamada da função simplex para resolver o problema de programação linear 
+solucao, valorOtimo, precoSombra = simplex(funcObj, restric, const)
+
+#Imprimindo os resultados solicitados
+print(f'Solução Ótima: {solucao}')
+print(f'Lucro Ótimo: {valorOtimo}')
+print(f'Preços Sombra: {precoSombra}')
